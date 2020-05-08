110bbff2d4a2fade0a6b93645ae6e2f80501d4b2
jack-mil <mcrajajack@gmail.com>
2020-05-08 17:54:19 +0000

    setup

--- main.py
+++ main.py
@@ -3,31 +3,31 @@ import pygame
 from settings import Settings
 
 
-WHITE = (200, 200, 200)
-RED = (255,0,0)
-
 def runGame():
+  RED = (255,0,0)
+  BLACK = (0,0,0)
+  WHITE = (255, 255, 255)
+  
   pygame.init()
   values = Settings()
   screen = pygame.display.set_mode((values.screen_width,values.screen_height))
-  screen.fill(values.bg_color)
+  screen.fill(WHITE)
+
+  def drawGrid():
+    blockSize = 20 #Set the size of the grid block
+    for x in range(values.screen_width):
+          for y in range(values.screen_height):
+              rect = pygame.Rect(x*blockSize, y*blockSize, blockSize, blockSize)
+              pygame.draw.rect(screen, BLACK, rect, 1)
 
   while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         sys.exit()
-    screen.fill(RED)
-    # drawGrid()
+    drawGrid()
+    pygame.display.flip()    
 
-  pygame.display.flip()    
     
-def drawGrid():
-  values = Settings()
-  blockSize = 20 #Set the size of the grid block
-  for x in range(values.screen_width):
-        for y in range(values.screen_height):
-            rect = pygame.Rect(x*blockSize, y*blockSize, blockSize, blockSize)
-            pygame.draw.rect(screen, WHITE, rect, 1)
 
 if __name__ == "__main__":
   runGame()
