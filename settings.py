7854a73b50ec82be5a939728743adac0ea68bfba
jack-mil <62065280+jack-mil@users.noreply.github.com>
2020-05-09 22:02:17 -0400

    refactored and reorganized some additions
    still trying to understand this git thing

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 class Settings():
   def __init__(self):
-    self.size = self.screen_width, self.screen_height = 1200, 800
+    self.size = self.screen_width, self.screen_height = 800, 600
+    self.block_size = 20
     self.bg_color = (230,230,0)
