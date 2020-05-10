67948f146a3f06da521e16d9b9dede49207ae634
jack-mil <mcrajajack@gmail.com>
2020-05-10 03:09:53 +0000

    can not hold and drag to pain cells

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import math
 from settings import Settings
 
 # GLOBALS
-settings = Settings()
+s = Settings()
 BLACK = (50, 50, 50)
 WHITE = (255, 255, 255)
 TEAL = (0, 185, 185)
@@ -14,16 +14,16 @@ MAGENTA = (200, 0, 100)
 def runGame():
 
     pygame.init()
-    screen = pygame.display.set_mode(
-        (settings.screen_width, settings.screen_height))  # rel.it runs 800x600
+    # repl.it runs 800x600, set in Settings().size
+    screen = pygame.display.set_mode(s.size, flags=pygame.RESIZABLE)  
     pygame.display.set_caption("The Game of Life")
     screen.fill(WHITE)
 
-    for x in range(settings.screen_width):
-        for y in range(settings.screen_height):
-            rect = pygame.Rect(x * settings.block_size,
-                               y * settings.block_size, settings.block_size,
-                               settings.block_size)
+    for x in range(s.screen_width):
+        for y in range(s.screen_height):
+            rect = pygame.Rect(x * s.block_size,
+                               y * s.block_size, s.block_size,
+                               s.block_size)
             pygame.draw.rect(screen, BLACK, rect, 1)
 
     while True:
@@ -35,33 +35,35 @@ def runGame():
                     sys.exit()  # quit on <ESC>
 
         # mouse click event to place a square
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            cell = getRectFromMouse()
+        # this method allows for press and hold to paint the screen
+        left, middle, right = pygame.mouse.get_pressed()
 
-            if event.button == pygame.BUTTON_LEFT:
-                pygame.draw.rect(screen, TEAL, cell)
+        # get cell the mouse is over currently
+        cell = getRectFromMouse()
 
-            if event.button == pygame.BUTTON_RIGHT:
-                pygame.draw.rect(screen, MAGENTA, cell)
+        # color cell according to pressed button
+        if left:
+            pygame.draw.rect(screen, TEAL, cell)
 
-            if event.button == pygame.BUTTON_MIDDLE:
-                pygame.draw.rect(screen, WHITE, cell)
-                pygame.draw.rect(screen, BLACK, cell, 1)
+        if right:
+            pygame.draw.rect(screen, MAGENTA, cell)
+            
+        if middle:
+            pygame.draw.rect(screen, WHITE, cell)
+            pygame.draw.rect(screen, BLACK, cell, 1)
 
         pygame.display.flip()
 
 
 def getRectFromMouse():
     """ 
-  Returns a pygame rectangle object located under the mouse 
-  Uses block_size defined in Settings.py
-  """
-
-    pos = pygame.mouse.get_pos()  # pos is an (x, y) coordinate array
-    posX = math.floor(pos[0] / settings.block_size
-                      ) * settings.block_size  # rounds to nearest block corner
-    posY = math.floor(pos[1] / settings.block_size) * settings.block_size
-    square = pygame.Rect(posX, posY, settings.block_size, settings.block_size)
+    Returns a pygame rectangle object located under the mouse 
+    Uses block_size defined in Settings.py
+    """
+    x, y = pygame.mouse.get_pos()  # pos is an (x, y) coordinate
+    posX = math.floor(x / s.block_size) * s.block_size  # rounds to nearest block corner
+    posY = math.floor(y / s.block_size) * s.block_size
+    square = pygame.Rect(posX, posY, s.block_size, s.block_size)
     return square
 
 
