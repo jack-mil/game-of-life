92cf91f3b4995c68158a78eb634227e93fb94cb6
00goldfish00 <65156062+00goldfish00@users.noreply.github.com>
2020-05-13 12:59:23 -0400

    moved runGame globals to top

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import sys
 import pygame
 import math
-import numpy as np
-from settings import Settings
-from cell_rules import GameOfLife, LIVE, DEAD
+import time
+from Settings import Settings
+from Rules import GameOfLife, LIVE
 
-# GLOBALS
+'''
+    Standard python style guide suggestions:
+    classes use CapitalCase
+    methods/functions use lowerCase
+    variables use lower_case
+    "Constants" use CAPS
+'''
+
+# GLOBALS #
 s = Settings()
 BLACK = (50, 50, 50)
 WHITE = (255, 255, 255)
@@ -13,111 +21,139 @@ TEAL = (0, 185, 185)
 MAGENTA = (200, 0, 100)
 
 screen = pygame.display.set_mode(s.size, flags=pygame.RESIZABLE)
-# screen should be global bc it's easier
-# repl.it runs 800x600, set in Settings().size
+''' screen should be global bc it's easier
+    repl.it runs 800x600, set in Settings().size '''
 pygame.display.set_caption("The Game of Life")
 
 habitats = s.population
 
+pygame.init()
+
 
 def runGame():
-    pygame.init()
-
     screen.fill(WHITE)
+    generation_counter = 0
+    started = False
+    paused = False
+    gen_speed = 0.5
 
-    # Draw initial board
+    ''' Draw initial grid '''
     for x in range(s.screen_width):
         for y in range(s.screen_height):
             rect = pygame.Rect(x * s.block_size, y * s.block_size,
                                s.block_size, s.block_size)
             pygame.draw.rect(screen, BLACK, rect, 1)
 
-    started = False
-    initial = set()
-    # Game loop
+    # Game loop #
     while True:
         for event in pygame.event.get():
+            ''' Quit on close or <ESC> '''
             if event.type == pygame.QUIT:
+                print("exiting")
                 sys.exit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    sys.exit()  # quit on <ESC>
+                    print("exiting")
+                    sys.exit()
 
-            if event.type == pygame.KEYDOWN:  # start on <RETURN> press for now
+            if event.type == pygame.KEYDOWN:
+
+                ''' Start by pressing <RETURN> '''
                 if event.key == pygame.K_RETURN:
-                    gol = GameOfLife(habitats.shape[0], habitats.shape[1],
-                                     initial)
+                    gol = GameOfLife(habitats)
                     started = True
                     print("the game of life has begun")
 
-                if event.key == pygame.K_RIGHT:
-                    gol.evolve()
-                    print("evolving")
+                ''' Pause/Un-pause by pressing <SPACE> '''
+                if event.key == pygame.K_SPACE:
+                    paused = not paused
+                    if paused:
+                        print("paused")
+                    else:
+                        print("resuming")
 
-        # Allow user to change board state if game hasn't started
+                ''' Manually evolve when game is paused '''
+                if started and paused:
+                    if event.key == pygame.K_RIGHT:
+                        gol.evolve()
+                        generation_counter += 1
+                        print(f"evolving to generation {generation_counter}")
+                        drawGeneration(gol.cells)
+
+            # if event.type == pygame.MOUSEWHEEL:       # Speed change not working yet
+
+        # Edit initial board state #
         if not started:
-            # mouse click event to place a square
-            # this method allows for press and hold to paint the screen
+            ''' Click/Press and hold mouse to edit '''
             left, middle, right = pygame.mouse.get_pressed()
 
-            # get cell the mouse is over currently
+            ''' Get cell that mouse is currently above '''
             cell_pos = getClickPos()
 
-            # color cell according to pressed button
             if left:
-                initial.add(cell_pos)
+                ''' Add LIVE cell to initial array '''
+                habitats[cell_pos[0], cell_pos[1]] = 1
                 print(f"Selected cell {cell_pos}")
-                habitats[cell_pos] = 1  # sets drawn cell as live in habitat array
-
-            # if right:
-            #     drawCell(cp, MAGENTA)
 
             if middle:
-                initial.remove(cell_pos)
-                habitats[
-                    cell_pos] = 0  # sets drawn cell as dead in habitat array
+                ''' Remove DEAD cell from initial array '''
+                habitats[cell_pos[0], cell_pos[1]] = 0
+                print(f"Removed cell {cell_pos}")
 
-            # draws cell for every living cell found in habitats array
-            for row in range(0, habitats.shape[0]):
-                for col in range(0, habitats.shape[1]):
-                    if habitats[row, col] == 1:
-                        drawCell((row, col), TEAL)
-                    else:
-                        drawCell((row, col), WHITE)
-                        drawCell((row, col), BLACK, 1)
-        else: # See this copy pasted code? yeah, that's what i did lol. there has got to be a better way
-            for i, row in enumerate(gol.cells):
-                for j, cell in enumerate(row):
-                    if cell.state == LIVE:
-                        drawCell((i, j), TEAL)
-                    else:
-                        drawCell((i, j), WHITE)
-                        drawCell((i, j), BLACK, 1)
+            # if right:  # maybe have three states in the future ?
+            #     habitats[cell_pos[0], cell_pos[1]] = 2
+
+            ''' Draw LIVE cells found in habitats '''
+            drawGeneration(habitats)
+
+        # Evolve in timed intervals #
+        elif not paused:
+            time.sleep(gen_speed)
+            gol.evolve()
+            generation_counter += 1
+            print(f"evolving to generation {generation_counter}")
+
+            ''' Draws LIVE cells found in gol.cells '''
+            drawGeneration(gol.cells)
+
         pygame.display.flip()
 
 
 def getClickPos():
-    """
-    Returns whole cell position tuple for the cell under the mouse
-    Uses block_size defined in Settings.py
-    """
-    x, y = pygame.mouse.get_pos(
-    )  # get_pos returns an (x, y) tuple of resolution
-    cellX = math.floor(
-        x / s.block_size)  # converts resolution to whole cell number
-    cellY = math.floor(y / s.block_size)
-    return cellX, cellY
+    """ Returns whole cell position tuple for the cell under the mouse
+        Uses block_size defined in Settings.py """
+
+    x, y = pygame.mouse.get_pos()  # get_pos returns an (x, y) tuple of resolution
+    pos_x = math.floor(x / s.block_size)  # converts resolution to whole cell number
+    pos_y = math.floor(y / s.block_size)
+    return pos_x, pos_y
 
 
 def drawCell(position, color, border=0):
-    """
-    draws a pygame rectangle at the given (x, y) position tuple
-    Uses block_size defined in Settings.py
-    """
-    X = position[0] * s.block_size  # converts cell to rounded resolution
-    Y = position[1] * s.block_size
-    cell = pygame.Rect(X, Y, s.block_size, s.block_size)
+    """ Draws a pygame rectangle at the given (x, y) position tuple
+        Uses block_size defined in Settings.py """
+
+    x = position[0] * s.block_size  # converts cell to rounded resolution
+    y = position[1] * s.block_size
+    cell = pygame.Rect(x, y, s.block_size, s.block_size)
     pygame.draw.rect(screen, color, cell, border)
+
+
+def drawGeneration(gen):
+    for y, row in enumerate(gen):
+        for x, col in enumerate(row):
+            if col == LIVE:
+                drawCell((y, x), TEAL)
+            else:
+                try:
+                    if col.state == LIVE:
+                        drawCell((y, x), TEAL)
+                    else:
+                        drawCell((y, x), WHITE)
+                        drawCell((y, x), BLACK, 1)
+                except AttributeError:
+                    drawCell((y, x), WHITE)
+                    drawCell((y, x), BLACK, 1)
 
 
 if __name__ == "__main__":
